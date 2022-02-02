60fb4dec0a799c4dbb1b75153ecccacfa9c4ac64
Guaderic <VladymyrGolovan@gmail.com>
2022-02-02 10:54:36 +0200

    add lesson1

---
+++ Lesson1/homework/main.js
@@ -0,0 +1,192 @@
+//- Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//Вивести кожну змінну за допомогою: console.log , alert, document.write
+/*let site = 'owu';
+console.log(site)
+alert(site)
+document.write(site)
+
+let str2 = 'com';
+console.log(str2)
+alert(str2)
+document.write(str2)
+
+let str3 = 'ua';
+console.log(str3)
+alert(str3)
+document.write(str3)
+
+let numInt = 1;
+console.log(numInt)
+alert(numInt)
+document.write(numInt)
+
+let numInt2 = 10;
+console.log(numInt2)
+alert(numInt2)
+document.write(numInt2)
+
+let negNumInt = -999;
+console.log(negNumInt)
+alert(negNumInt)
+document.write(negNumInt)
+
+let numInt3 = 123;
+console.log(numInt3)
+alert(numInt3)
+document.write(numInt3)
+
+let numFloat = 3.14;
+console.log(numFloat)
+alert(numFloat)
+document.write(numFloat)
+
+let numFloat2 = 2.7;
+console.log(numFloat2)
+alert(numFloat2)
+document.write(numFloat2)
+
+let numInt4 = 16;
+console.log(numInt4)
+alert(numInt4)
+document.write(numInt4)
+
+let bool1 = true;
+console.log(bool1)
+alert(bool1)
+document.write(bool1)
+
+let bool2 = false;
+console.log(bool2)
+alert(bool2)
+document.write(bool2)
+
+//- Переприсвоїти кожній змінній з завдання значення на довільне.
+ // Вивести кожну змінну за допомогою: console.log , alert, document.write
+let str = 'world';
+console.log(str)
+alert(str)
+document.write(str)
+
+
+let site = 'rozetka';
+console.log(site)
+alert(site)
+document.write(site)
+
+let str2 = 'org';
+console.log(str2)
+alert(str2)
+document.write(str2)
+
+let str3 = 'uk';
+console.log(str3)
+alert(str3)
+document.write(str3)
+
+let numInt = 2;
+console.log(numInt)
+alert(numInt)
+document.write(numInt)
+
+let numInt2 = 20;
+console.log(numInt2)
+alert(numInt2)
+document.write(numInt2)
+
+let negNumInt = -199;
+console.log(negNumInt)
+alert(negNumInt)
+document.write(negNumInt)
+
+let numInt3 = 321;
+console.log(numInt3)
+alert(numInt3)
+document.write(numInt3)
+
+//let numFloat = 1.7;
+console.log(numFloat)
+alert(numFloat)
+document.write(numFloat)
+
+//let numFloat2 = 5.6;
+console.log(numFloat2)
+alert(numFloat2)
+document.write(numFloat2)
+
+//let numInt4 = 21;
+console.log(numInt4)
+alert(numInt4)
+document.write(numInt4)
+
+//let bool1 = true;
+console.log(bool1)
+alert(bool1)
+document.write(bool1)
+
+//let bool2 = false;
+console.log(bool2)
+alert(bool2)
+document.write(bool2)*/
+
+//- Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+//let firstName = 'Головань';
+//let middleName = 'Володимир';
+//let lastName = 'Юрійович';
+//let person = firstName + ' ' + middleName + ' ' + lastName;
+
+
+//- За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+   // Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+//let middleName = prompt("Введіть ваше ім'я");
+//let lastName = prompt("Введіть ваше ім'я по батькові");
+//let age = prompt("Скільки вам років?");
+//alert("Привіт " + middleName + " " + lastName + ". Тобі " + age + " років.");
+
+//- За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+   // let a = 100; let b = '100'; let c = true;
+
+/*let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);*/
+
+
+//- Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+    //В однакових виразах не використовувати однакові оператори!!!
+
+/*let n1 = 5 < 6;
+let n2 = 5 > 6;
+let n3 = 5 === 6;
+let n4 = 5 == 6;
+console.log(n4);
+
+let num1 = 10 >= 10;
+console.log(num1);
+let num2 = 10 === 10;
+console.log(num2);
+let num3 = 10 !== 10;
+console.log(num3);
+let num4 = 10 < 10;
+console.log(num4);
+let num5 = 10 < 10;
+console.log(num5);
+let num6 = 123 === '123';
+console.log(num6);
+let num7 = 123 == '123';
+console.log(num7);*/
+
+
+//- Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+let str = "20";
+let a = 5;
+document.write(str + a + "<br/>");
+
+//Відбувається конкатенація рядка.
+
+document.write(str - a + "<br/>");
+
+document.write(str * "2" + "<br/>");
+document.write(str / 2 + "<br/>");
