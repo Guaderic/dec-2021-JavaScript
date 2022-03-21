4e25b4a614ca0403deb25067a090d52208618f7f
Guaderic <VladymyrGolovan@gmail.com>
2022-03-21 11:39:34 +0200

    add lesson9

--- project/main-user-details.js
+++ project/main-user-details.js
@@ -1,11 +1,27 @@
 let url = new URL(location.href);
 let user =JSON.parse(url.searchParams.get('data'))
-let userDetailsContainer = document.getElementsByClassName('user-details')[0];
-userDetailsContainer.innerText = `${JSON.stringify(user)}`
+function userDetail(point){
+    for (const pointKey in point) {
+        if(typeof point[pointKey] !== 'object'){
+            let userContainer = document.createElement('div');
+            userContainer.innerText = point[pointKey];
+            userContainer.classList.add('user-details');
+            document.body.append(userContainer);
+
+        }else if(typeof point[pointKey] === 'object'){
+            userDetail(point[pointKey])
+        }
+    }
+}
+userDetail(user);
+
+
+
 let postsDetailsButton = document.getElementsByClassName('postsDetailsButton')[0];
 postsDetailsButton.innerText = "show user's posts"
 let postsContainer = document.getElementsByClassName('posts')[0];
 postsDetailsButton.onclick = function (){
+        this.disabled = true;
     fetch(`https://jsonplaceholder.typicode.com/users/${user.id}/posts`)
         .then(value => (value.json())
         .then(value => {
@@ -13,9 +29,9 @@ postsDetailsButton.onclick = function (){
                 let postContainer = document.createElement('div');
                 postContainer.classList.add('post');
                 postContainer.innerText = postItem.title;
-                postsContainer.append(postContainer);
                 let postButton = document.createElement('button');
                 postContainer.appendChild(postButton);
+                postsContainer.appendChild(postContainer);
                 postButton.innerText = 'post details'
                 postButton.onclick = function (){
                     location.href = `./post-details.html?data=${JSON.stringify(postItem)}`;
@@ -24,4 +40,3 @@ postsDetailsButton.onclick = function (){
             }
         }))
 }
-
